28cffbaa39fbaa02f895f435f5c8d033e892b210
Kesa Sai Prasanth Kumar <40555740+KSPK542@users.noreply.github.com>
2023-01-26 22:11:26 -0600

    Add files via upload

---
+++ MaxNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MaxNumber {
+  public static void main(String[] args) {
+    int num1, num2;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter first number:");
+    num1 = scanner.nextInt();
+    System.out.println("Enter second number:");
+    num2 = scanner.nextInt();
+    scanner.close();
+    if(num1 > num2)
+        System.out.println(num1 + " is greater");
+    else
+        System.out.println(num2 + " is greater");
+  }
+}
