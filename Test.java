84ce1e17dc816bbddc7ed353fe91990368a4adf5
Kesa Sai Prasanth Kumar <40555740+PrasanthKesa@users.noreply.github.com>
2023-01-30 19:49:20 -0600

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.lang.*;
+
+class Test {
+	public static void main(String[] args)
+	{
+		String s = "Prasanth";
+
+		System.out.println("This is " + s);
+
+		String s1 = new String("PRASANTH");
+
+		System.out.println("THIS IS " + s1);
+	}
+}
